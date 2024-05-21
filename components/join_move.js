c437fb10266f2b704a3c9bcf703c73e49b73b04e
Cameron Keith <cameron.s.keith.26@dartmouth.edu>
2024-05-20 21:47:21 -0400

    added create_move and join_move placeholders

---
+++ components/join_move.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image, Button, TouchableOpacity } from 'react-native';
+import { BackgroundImage } from 'react-native-elements/dist/config';
+
+const JoinMove = () => {
+    const handleCreateMove = () => {
+        console.log('Create Move');
+    }
+    const handleJoinMove = () => {
+        console.log('Join Move');
+    }
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}> JoinMove </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingBottom: 50,
+    flex: 1,
+    direction: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    gap: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+    fontStyle: 'italic',
+  },
+
+  button: {
+    borderWidth: 3,
+    borderColor: 'black',
+    justifySelf: 'center',
+    alignSelf: 'center',
+    width: 200,
+    borderRadius: 20,
+    padding: 10,
+    text: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+  }
+});
+
+export default JoinMove;
