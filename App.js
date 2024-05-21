059faea8d129c4795bc8809b0dd3f942d4e715f5
Shaamil <shaamil101@gmail.com>
2024-05-20 21:42:50 -0400

    Adding create_move without data passing yet

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { LogBox } from "react-native";
 import MainTabBar from './navigation/main_tab_bar';
 import Welcome from './components/welcome';
-
+import CreateMove from './components/create_move';
 // disable really annoying in app warnings
 LogBox.ignoreAllLogs();
 
 const App = (props) => {
-  return <Welcome />;
+  return <CreateMove/>;
 };
 
 
